0936c7b4a63077f53e392d0acfbb2033a1d1d8ad
Artur Pluzhnikov <artur@artbox-btl.ru>
2022-08-29 00:25:29 +0300

    15th project, 2nd iterration

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const { auth } = require('./middlewares/auth');
 const NotFound = require('./errors/NotFound');
 const { errorHandler } = require('./errors/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-// const cors = require('./middlewares/cors');
+const cors = require('./middlewares/cors');
 
 const PORT = process.env.PORT || 3000;
 
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
-// app.use(cors());
+app.use(cors());
 
 app.get('/crash-test', () => {
   setTimeout(() => {
