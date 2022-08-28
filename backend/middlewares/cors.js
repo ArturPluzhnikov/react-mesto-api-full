08d29b6f1c19fd693f95e9437ed443a72e7fa7a2
Artur Pluzhnikov <artur@artbox-btl.ru>
2022-08-29 00:21:29 +0300

    15th project, 2nd iterration

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,27 +1,27 @@
-// const allowedCors = [
-//   'https://artur.artbox.nomoredomains.sbs',
-//   'https://api.arturartbox.students.nomoredomains.sbs',
-//   'http://localhost:3000',
-//   'http://artur.artbox.nomoredomains.sbs',
-//   'http://api.arturartbox.students.nomoredomains.sbs',
-// ];
+const allowedCors = [
+  'https://artur.artbox.nomoredomains.sbs',
+  'https://api.arturartbox.students.nomoredomains.sbs',
+  'http://localhost:3000',
+  'http://artur.artbox.nomoredomains.sbs',
+  'http://api.arturartbox.students.nomoredomains.sbs',
+];
 
-// module.exports = ((req, res, next) => {
-//   const { origin } = req.headers;
-//   const { method } = req;
+module.exports = ((req, res, next) => {
+  const { origin } = req.headers;
+  // const { method } = req;
 
-//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-//   const requestHeaders = req.headers['access-control-request-headers'];
+  // const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  // const requestHeaders = req.headers['access-control-request-headers'];
 
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//     res.header('Access-Control-Allow-Credentials', true);
-//   }
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     res.status(200).send();
-//     return;
-//   }
-//   next();
-// });
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', "*");
+    // res.header('Access-Control-Allow-Credentials', true);
+  }
+  // if (method === 'OPTIONS') {
+  //   res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  //   res.header('Access-Control-Allow-Headers', requestHeaders);
+  //   res.status(200).send();
+  //   return;
+  // }
+  next();
+});
