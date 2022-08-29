f21cdacfa689281a151197ac28385e7cfee357e1
Artur Pluzhnikov <artur@artbox-btl.ru>
2022-08-29 11:54:07 +0300

    15th project, 2nd iterration

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ const { auth } = require('./middlewares/auth');
 const NotFound = require('./errors/NotFound');
 const { errorHandler } = require('./errors/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { allowedCors } = require('./utils/allowedCors');
 
 const PORT = process.env.PORT || 3000;
 
@@ -20,10 +21,8 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+
+app.use(cors(allowedCors));
 
 app.use(requestLogger);
 
---
+++ backend/utils/allowedCors.js
@@ -0,0 +1,12 @@
+const allowedCors = {
+  origin: [
+    'http://artur.artbox.nomoredomains.sbs/',
+    'https://artur.artbox.nomoredomains.sbs/',
+    'http://api.arturartbox.students.nomoredomains.sbs/',
+    'https://api.arturartbox.students.nomoredomains.sbs/',
+    'https://locahost:3000',
+    'http://locahost:3000',
+  ],
+};
+
+module.exports = allowedCors;
